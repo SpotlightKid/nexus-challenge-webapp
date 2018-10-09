de901f13d4e29bdd06e9abb5b9ec71aa4067074e
Christopher Arndt <chris@chrisarndt.de>
2018-10-09 15:00:18 +0200

    Add Prizes page to nav menu
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- fmchallengewebapp/settings.py
+++ fmchallengewebapp/settings.py
@@ -47,6 +47,7 @@ NAVIGATION_LINKS = (
     ("/rules/", "Rules"),
     ("/submit/", "Enter"),
     ("/vote/", "Voting"),
+    ("/prizes/", "Prizes"),
     ("/fmsynths/", "FM Synths"),
     ("mailto:fmchallenge@osamc.de", "Contact"),
     #("/blog/", "Blog"),
